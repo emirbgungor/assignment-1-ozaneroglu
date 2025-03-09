1166f633a96d531d94926ba5cfd71f6bff42f494
Emir Burak Güngör <emir.gungor@std.bogazici.edu.tr>
2025-03-09 12:39:15 +0300

    assignment1

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
-
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+    romen=''
+    while num > 0:
+        if num >= 1000:
+            romen += 'M' * (num//1000)
+            num = num % 1000
+        elif num>=900:
+            romen += 'CM'
+            num=num % 900
+        elif num >= 500:
+            romen += 'D'
+            num = num % 500
+        elif num >= 400:
+            romen += 'CD'
+            num = num % 400
+        elif num >= 100:
+            romen += 'C'* (num//100)
+            num = num % 100
+        elif num >= 90:
+            romen += 'XC'
+            num = num % 90
+        elif num >= 50:
+            romen += 'L'
+            num = num % 50
+        elif num >= 40:
+            romen += 'XL'
+            num = num % 40
+        elif num >= 10:
+            romen += 'X' * (num//10)
+            num = num % 10
+        elif num >= 9:
+            romen += 'IX'
+            num = num % 9
+        elif num >= 5:
+            romen += 'V'
+            num = num % 5
+        elif num >= 4:
+            romen += 'IX'
+            num = num % 4
+        elif num>=1:
+            romen += 'I' * (num//1)
+            break
+    return romen         
--- main_test.py
+++ main_test.py
@@ -6,5 +6,3 @@ from main import int_to_roman
 def test_int_to_roman_3():
     assert int_to_roman(3) == "III"
 
-
-
